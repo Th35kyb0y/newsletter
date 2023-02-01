150c994df8d23a7d1176e53032aec4de8d7dddb0
deepak mishra <38756178+Th35kyb0y@users.noreply.github.com>
2023-02-01 11:12:10 +0530

    Update editor.js

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -48,7 +48,7 @@ export default function EditorPage() {
   };
 
   const sendLetter = async (value, emails) => {
-    await postLetter(value, emails).then(res => { alert(res) }).catch(err => { alert(err) })
+    await postLetter(value, emails).then(res => { alert("success",res) }).catch(err => { alert("error response",err) })
   }
 
 
