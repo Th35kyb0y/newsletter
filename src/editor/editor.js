5c223aa3b8b784a2c2088af9ca7e3433b3418258
deepak mishra <38756178+Th35kyb0y@users.noreply.github.com>
2023-02-01 11:01:14 +0530

    Update editor.js

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -48,7 +48,7 @@ export default function EditorPage() {
   };
 
   const sendLetter = async (value, emails) => {
-    await postLetter(value, emails).then(res => { console.log(res) }).catch(err => { console.log(err) })
+    await postLetter(value, emails).then(res => { alert("template has sent to backend") }).catch(err => { alert("error occured while sending template to backend") })
   }
 
 
